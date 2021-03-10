7f91c3dc2e3dca2a2e37c0044122750585e2f938
Pascal Knüppel <captain.p.goldfish@gmx.de>
2021-03-10 14:24:24 +0100

    Setup minimal-test-project

---
+++ src/main/java/de/captaingoldfish/docker/issue/Hello.java
@@ -0,0 +1,14 @@
+package de.captaingoldfish.docker.issue;
+
+/**
+ * @author Pascal Knüppel
+ * @since 10.03.2021
+ */
+public class Hello
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
